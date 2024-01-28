2f24b4a03c1932370b3a88fefd9549656aac0c62
eric98zhang <eric99.zhang@gmail.com>
2024-01-28 06:49:47 +0000

    Change login page text

--- app/(paths)/locked/login/page.tsx
+++ app/(paths)/locked/login/page.tsx
@@ -8,8 +8,11 @@ export default async function Page() {
     <main className="mx-3 my-4 flex flex-grow flex-col">
       <div className="mx-auto flex max-w-2xl flex-grow flex-col">
         <div className="text-sm">
-          Sign in using a link sent to your email address. Sign-ups are closed
-          (contact me).
+          This sign-in page is slightly broken. After a successful sign-in, it
+          WON'T automatically redirect you. Click the 'LOCKED' tab once more
+          after clicking the 'Sign in'. 
+          
+          Sign-ups are closed.
         </div>
         <div className="my-4 flex flex-grow flex-col justify-around">
           <div>
